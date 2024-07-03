1872fd0cf27fd40916a69cdc7dcd2d2f7d305d94
Anwar Mujeeb <ibnmujeeb2003@gmail.com>
2024-07-03 12:34:56 -0700

    updated main.py for vectorstore

--- github.py
+++ github.py
@@ -8,12 +8,17 @@ load_dotenv()
 github_token = os.getenv("GITHUB_TOKEN")
 
 def fetch_github(owner, repo, endpoint):
+
+    # Craft URL
     url = f"https://api.github.com/repos/{owner}/{repo}/{endpoint}"
+
+    # Make request
     headers = {"Authorization": f"Bearer {github_token}"}
     response = requests.get(url, headers=headers)
 
+    # Successful request
     if response.status_code == 200:
-        data = response.json()
+        data = response.json() # Dictionary 
     else:
         print("Failed with status code:", response.status_code)
         return []
@@ -28,8 +33,14 @@ def fetch_github_issues(owner, repo):
 
 
 def load_issues(issues):
+    """
+    Load issues into Langchain Document objects to use in RAG
+    """
     docs = []
+
+    # Iterate through issues
     for entry in issues:
+        # Extract metadata into dictionary
         metadata = {
             "author": entry["user"]["login"],
             "comments": entry["comments"],
@@ -44,3 +55,9 @@ def load_issues(issues):
         docs.append(doc)
 
     return docs
+
+
+owner = "techwithtim"
+repo = "Flask-Web-App-Tutorial"
+endpoint = "issues"
+fetch_github_issues(owner, repo)
